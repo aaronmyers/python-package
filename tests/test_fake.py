ba9974f3bc3f899c0c3f91883abea639f099bd7a
AaronMyers <aaron.n.myers@gmail.com>
2019-08-24 21:34:49 -0500

    updating tests

--- tests/test_fake.py
+++ tests/test_fake.py
@@ -20,8 +20,6 @@ class TestFake(unittest.TestCase):
 
     def test_something(self):
         output = fake.hello()
-        print(self.message)
-        print(output)
         assert(output == self.message)
 
     def tearDown(self):
